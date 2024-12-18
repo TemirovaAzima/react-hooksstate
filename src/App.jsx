95422be120635cb8883db96cf544cf1f030210da
TemirovaAzima <temirovaazima9@gmail.com>
2024-12-18 12:20:22 +0500

    hooks with objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import {useState} from 'react'
 
  export const App = ()=>{
-    const [dostlar, setDostlar]= useState(['Lisa', 'Jennie'])
-     const addFriend= ()=> setDostlar([...dostlar, 'Rosie'])
-     const removeFriend = ()=> setDostlar(dostlar.filter((value)=> value !=='Rosie'))
-     const friendUpdateInfo=()=>setDostlar(dostlar.map((value)=> (value==="Rosie")? 'Rosie Bruna Mars' : value))
-    return (
-        <section>
-            {dostlar.map((dost)=>(
-                <p key={Math.random()}>{dost}</p>
-            ))}
-            <button onClick={addFriend}>add new friend</button>
-            <button onClick={removeFriend}>remove friend</button>
-            <button onClick={friendUpdateInfo}>Friend update names</button>
-        </section>
-    )
+      const [kino, setKino]= useState({
+          title: "InterStellel",
+          rating: "9"
+      })
+     // const changeRating= ()=>setKino({...kino, rating: '7'})
+     const changeRating = ()=> setKino((prevKino)=>({...prevKino, rating:'4' }));
+     return(
+         <section>
+             <h1>Kino nomi: {kino.title}</h1>
+             <p>Kino rating: {kino.rating}</p>
+             <button onClick={changeRating}>Change rating</button>
+         </section>
+     )
  }
