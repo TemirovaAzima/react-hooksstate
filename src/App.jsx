7a4ebf55f0cfaaeb5fa05a6b90045a952316384b
TemirovaAzima <temirovaazima9@gmail.com>
2024-12-18 12:47:54 +0500

    hooks with array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import {useState} from 'react'
 
  export const App = ()=>{
-      const [kino, setKino]= useState({
-          title: "InterStellel",
-          rating: "9"
-      })
+      const [kinolar, setKinolar]= useState([
+          {id:1, title: 'InterStellar', rating:9},
+          {id:2, title: 'Spiderman', rating:8},
+          {id:3, title: 'Joker', rating:7}
+      ])
      // const changeRating= ()=>setKino({...kino, rating: '7'})
-     const changeRating = ()=> setKino((prevKino)=>({...prevKino, rating:'4' }));
+     // const changeRating = ()=> setKino((prevKino)=>({...prevKino, rating:'4' }));
+     // const changeTitle = ()=>setKinolar(kinolar.map((movie)=>(movie.id===2 ? {...movie, title: 'BatmanHehe'}: movie)))
+     const changeTitle = ()=> setKinolar((prevKino)=>prevKino.map((movie)=> (movie.id===1? {...movie, title:'Jenniefer'}: movie)))
      return(
          <section>
-             <h1>Kino nomi: {kino.title}</h1>
-             <p>Kino rating: {kino.rating}</p>
-             <button onClick={changeRating}>Changge ratinghe</button>
+             {kinolar.map((kino) => (
+                 <div key={kino.id}>
+                     <span>{kino.id} kino nomi: {kino.title}</span>
+                     <span> kino rating :{kino.rating}</span>
+                 </div>
+             ))}
+             <button onClick={changeTitle}>Change title</button>
          </section>
      )
  }
